c8ea11b01ae4075f485db8aca70d167ed49c8ad0
chan1945 <heechan890@gmail.com>
2026-06-06 14:52:28 +0900

    모델 1 분류모델 추가

--- src/silverwalk_ai/modeling/__init__.py
+++ src/silverwalk_ai/modeling/__init__.py
@@ -1,21 +1,33 @@
 """모델 학습과 평가 유틸리티."""
 
 from silverwalk_ai.modeling.mlp import (
+    ACCIDENT_TARGET_COLUMN,
     MLPTrainingConfig,
+    add_accident_target,
+    build_accident_classifier_model,
     build_mlp_model,
     configure_tensorflow_device,
+    evaluate_accident_predictions,
     evaluate_predictions,
+    predict_accident_probability,
     predict_risk,
     predict_unlabeled_risk,
+    train_accident_classifier_model,
     train_mlp_model,
 )
 
 __all__ = [
+    "ACCIDENT_TARGET_COLUMN",
     "MLPTrainingConfig",
+    "add_accident_target",
+    "build_accident_classifier_model",
     "build_mlp_model",
     "configure_tensorflow_device",
+    "evaluate_accident_predictions",
     "evaluate_predictions",
+    "predict_accident_probability",
     "predict_risk",
     "predict_unlabeled_risk",
+    "train_accident_classifier_model",
     "train_mlp_model",
 ]
